f9efb723638025b6a0b7d3b58cc6e2096d2fcb72
SalmanIrsyad <salman_070@smkassalaambandung.sch.id>
2023-02-24 15:54:37 +0700

    Tahap 2 - 4

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,12 +1,39 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../dashboard/views/dashboard_view.dart';
+import '../../login/views/login_view.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
-  final count = 0.obs;
+  // final count = 0.obs;
+  late Timer _pindah;
+  final authToken = GetStorage();
+
   @override
   void onInit() {
     super.onInit();
+      _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => authToken.read('token') == null
+          ? Get.off(
+              () => const LoginView(),
+              transition: Transition.leftToRight,
+            )
+          : Get.off(() => const DashboardView()),
+    );
+
+
+    // _pindah = Timer.periodic(
+    //   const Duration(seconds: 4),
+    //   (timer) => Get.off(
+    //     () => const LoginView(),
+    //     transition: Transition.leftToRight,
+    //   ),
+    // );
   }
 
   @override
@@ -17,7 +44,12 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+    _pindah.cancel();
   }
 
-  void increment() => count.value++;
+  // void increment() => count.value++;
+}
+
+class _pindah {
+  static void cancel() {}
 }
---
+++ lib/app/utils/api.dart
@@ -0,0 +1,13 @@
+class BaseUrl {
+  static String auth = 'https://demo-elearning.smkassalaambandung.sch.id/api/login/';
+}
+
+
+
+// Berikut adalah penjelasan lebih detail tentang kode tersebut:
+
+// *. class BaseUrl { ... }: Mendefinisikan sebuah kelas dengan nama BaseUrl.
+// *. static: Kata kunci yang menunjukkan bahwa atribut auth adalah atribut statis, artinya atribut ini dimiliki oleh kelas dan bukan oleh instance dari kelas tersebut.
+// *. String: Tipe data dari atribut auth adalah String.
+// *. auth: Nama dari atribut yang didefinisikan.
+// *. 'https://demo-elearning.smkassalaambandung.sch.id/api/login/': Nilai awal dari atribut auth yang berupa sebuah URL endpoint API untuk melakukan autentikasi login. Nilai ini bersifat konstan karena menggunakan kata kunci static, sehingga tidak dapat diubah dari instance objek yang dibuat dari kelas tersebut.
