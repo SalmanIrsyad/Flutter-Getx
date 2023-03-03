af507e321916d934ed7b06df9ffcb53a22e97d7c
SalmanIrsyad <salman_070@smkassalaambandung.sch.id>
2023-03-03 15:19:47 +0700

    Build APK

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -11,8 +11,40 @@ import '../../../utils/api.dart';
 class DashboardController extends GetxController {
   //TODO: Implement DashboardController
 
-  final count = 0.obs;
+  // final count = 0.obs;
   final _getConnect = GetConnect();
+
+  //fungsi untuk mengambil headline dari server
+  Future<HeadlineResponse> getHeadline() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final response = await _getConnect.get(BaseUrl.headline);
+    //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  //fungsi untuk mengambil headline dari server
+Future<EntertainmentResponse> getEntertainment() async {
+	//memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+  final response = await _getConnect.get(BaseUrl.entertainment);
+	//mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+  return EntertainmentResponse.fromJson(jsonDecode(response.body));
+}
+//fungsi untuk mengambil headline dari server
+Future<SportsResponse> getSports() async {
+	//memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+  final response = await _getConnect.get(BaseUrl.sports);
+	//mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+  return SportsResponse.fromJson(jsonDecode(response.body));
+}
+//fungsi untuk mengambil headline dari server
+Future<TechnologyResponse> getTechnology() async {
+	//memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+  final response = await _getConnect.get(BaseUrl.technology);
+	//mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
+  return TechnologyResponse.fromJson(jsonDecode(response.body));
+}
+
+
   @override
   void onInit() {
     super.onInit();
@@ -28,35 +60,5 @@ class DashboardController extends GetxController {
     super.onClose();
   }
 
-  //fungsi untuk mengambil headline dari server
-  Future<HeadlineResponse> getHeadline() async {
-    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
-    final response = await _getConnect.get(BaseUrl.headline);
-    //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
-    return HeadlineResponse.fromJson(jsonDecode(response.body));
-  }
-
-  //fungsi untuk mengambil headline dari server
-Future<EntertainmentResponse> getEntertainment() async {
-	//memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
-  final response = await _getConnect.get(BaseUrl.headline);
-	//mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
-  return EntertainmentResponse.fromJson(jsonDecode(response.body));
-}
-//fungsi untuk mengambil headline dari server
-Future<SportsResponse> getSports() async {
-	//memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
-  final response = await _getConnect.get(BaseUrl.headline);
-	//mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
-  return SportsResponse.fromJson(jsonDecode(response.body));
-}
-//fungsi untuk mengambil headline dari server
-Future<TechnologyResponse> getTechnology() async {
-	//memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
-  final response = await _getConnect.get(BaseUrl.headline);
-	//mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
-  return TechnologyResponse.fromJson(jsonDecode(response.body));
-}
-
-  void increment() => count.value++;
+  // void increment() => count.value++;
 }
